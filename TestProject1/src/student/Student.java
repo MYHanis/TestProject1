3258ef0f4e38d2ad6f4492b285b2cfe8135a0dca
User <User@10.107.30.154>
2023-10-11 23:59:03 +0800

    added name,matric number, and address

---
+++ TestProject1/src/student/Student.java
@@ -0,0 +1,17 @@
+package student;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NURHANIS NABILAH BINTI MOHD NAJIB");
+		System.out.println("216520");
+		System.out.println("Kepala Batas,Penang");
+		
+		
+		
+		
+		
+	}
+
+}
