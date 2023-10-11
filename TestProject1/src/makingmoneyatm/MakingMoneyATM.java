66d14f76d0adcec8f27b8cfc50fbc5639ee33f95
User <User@10.107.30.154>
2023-10-12 00:29:28 +0800

    Making Money Atm

---
+++ TestProject1/src/makingmoneyatm/MakingMoneyATM.java
@@ -0,0 +1,20 @@
+package makingmoneyatm;
+
+public class MakingMoneyATM {
+
+			public static void main(String []args){
+
+			//prints output
+			System.out.println("+------------------------------------------------------------------+");
+			System.out.println("+                                                                  +");
+			System.out.println("+                    Making Money Bank ATM Service                 +");
+			System.out.println("+                                                                  +");
+			System.out.println("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+");
+			System.out.println();
+			System.out.println("Welcome!");
+			
+			System.out.println("by HanisNabilah, 12/10/23@12.28AM :)");
+			
+	}
+
+}
